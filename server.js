86cc8d014695d1f8358117d91982ae45c93a47fd
hlmgnc <61838067+hlmgnc@users.noreply.github.com>
2021-09-20 14:51:14 +0200

    Add mongodb connection

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const articleRouter = require('./routes/articles')
 const app = express()
 
 
-mongoose.connect('mongodb://localhost/daily', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://hlmgnc:<password>@cluster0.3s1ge.mongodb.net/dailyblog?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.set('view engine', 'ejs')
 
